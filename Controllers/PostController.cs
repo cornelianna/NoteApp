eceffe57ee185589c8729b6c9fbec1f14633c8c1
Amiin <amiin20004@gmail.com>
2024-09-25 13:30:10 +0200

    Basic UI chnages and added DELETE AND Update to the PostController and in the View

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -65,5 +65,85 @@ namespace NoteApp.Controllers
 
             return RedirectToAction("Index");
         }
+
+
+[Authorize]
+[HttpGet]
+public async Task<IActionResult> EditPost(int id)
+{
+    var post = await _context.Posts.FindAsync(id);
+
+    if (post == null)
+    {
+        return NotFound();
+    }
+
+  
+    if (post.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+    {
+        return Forbid();  
+    }
+
+    return View(post);  
+}
+
+[Authorize]
+[HttpPost]
+public async Task<IActionResult> EditPost(int id, Post updatedPost, IFormFile? newImage)
+{
+    var post = await _context.Posts.FindAsync(id);
+
+    if (post == null)
+    {
+        return NotFound();
+    }
+
+    
+    if (post.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+    {
+        return Forbid();
+    }
+
+    if (newImage != null && newImage.Length > 0)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", newImage.FileName);
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await newImage.CopyToAsync(stream);
+        }
+        post.ImageUrl = "/uploads/" + newImage.FileName;
+    }
+
+    post.Content = updatedPost.Content;
+    post.CreatedAt = DateTime.Now;  // Update timestamp, optional
+
+    _context.Update(post);
+    await _context.SaveChangesAsync();
+
+    return RedirectToAction("Index");
+}
+
+[Authorize]
+[HttpPost]
+public async Task<IActionResult> DeletePost(int id)
+{
+    var post = await _context.Posts.FindAsync(id);
+
+    if (post == null)
+    {
+        return NotFound();
+    }
+
+    // Ensure that only the user who created the post can delete it
+    if (post.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+    {
+        return Forbid();  // Return a 403 Forbidden response if the user is not the owner
+    }
+
+    _context.Posts.Remove(post);
+    await _context.SaveChangesAsync();
+
+    return RedirectToAction("Index");
+}
     }
 }
