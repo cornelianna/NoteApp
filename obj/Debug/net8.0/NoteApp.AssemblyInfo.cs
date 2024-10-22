d428c8b9fe33585f0e1697ec589812efe98810a0
Amiin <amiin20004@gmail.com>
2024-10-22 14:11:47 +0200

    Made changes on postcorntoller and major UI changes to make it easier to post and added CRUD in the Postcontroller.

--- obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+372bc60abb90c97c179d49b674f8fba57f0d2a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a876ee990ca6838cb3d9b9717c0de07fd8139b")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
