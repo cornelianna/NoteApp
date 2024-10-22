b2b645d20f9e3f27197f929eb23fa5575225e78a
nguyenmtm <nguyenmichael7795@gmail.com>
2024-10-22 15:33:48 +0200

    gitignore

--- obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0ddf8aa5f04bd385b342347a709940a4fc3b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48b926a2c027aafb261d79c74536af09d7d9c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
