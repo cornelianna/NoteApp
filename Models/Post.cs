824887d424f68460376b1a3b61f728a60427360d
cornelianna <cornelianna12@gmail.com>
2024-11-05 21:17:30 +0100

    Add validation for Post content to ensure it is required

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NoteApp.Models
 {
     public class Post
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Content is required.")]
         public string Content { get; set; } = string.Empty;
         public byte[]? ImageData { get; set; } // Image stored as byte array
         public DateTime CreatedAt { get; set; }
